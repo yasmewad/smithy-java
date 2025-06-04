61d9af6c917384d568e0e7c388b0d01881eac989
Adwait Kumar Singh <adwsingh@amazon.com>
2025-06-03 19:14:16 -0700

    Add more tests for BigDecimal/BigInteger cbor serde

--- codecs/cbor-codec/src/test/java/software/amazon/smithy/java/cbor/CborSerializerTest.java
+++ codecs/cbor-codec/src/test/java/software/amazon/smithy/java/cbor/CborSerializerTest.java
@@ -14,7 +14,11 @@ import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.HexFormat;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import software.amazon.smithy.java.io.ByteBufferUtils;
 
 public class CborSerializerTest {
@@ -45,6 +49,83 @@ public class CborSerializerTest {
         assertEquals(wingspan, de.wingspan);
         assertBuffersEqual(bytes, de.bytes);
         assertEquals("li'l guy", new String(ByteBufferUtils.getBytes(de.bytes)));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "1",
+            "-5",
+            "9223372036854775807",
+            "-9223372036854775807",
+            "1844674407370955161618446744073709551616",
+            "-922337203685477581318446744073709551616",
+            "9223372036854775813",
+            "1000000000000000000000000000000000000000000000000001",
+            "-9223372036854775936",
+            "18446744073709551616",
+            "-18446744073709551616",
+            "-9223372036854775808"
+    })
+    void testBigInteger(String input) {
+        var flightRange = new BigInteger(input);
+        var bird = new CborTestData.BirdBuilder().flightRange(flightRange).build();
+        var ser = CODEC.serialize(bird, SETTINGS);
+        var de = new CborTestData.BirdBuilder().deserialize(CODEC.newDeserializer(ser, SETTINGS)).build();
+        assertEquals(flightRange, de.flightRange);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "1",
+            "1.0",
+            "-5.0",
+            "18446744073709551616.18446744073709551616",
+            "-9223372036854775813.18446744073709551616",
+            "92233720.36854775813",
+            "-18446744073709551616",
+            "-0.18446744073709551616",
+            "0.18446744073709551616",
+            "18446744073709551616.18446744073709551616184467440737095516160",
+            "-18446744073709551616184467440.73709551616184467440737095516160",
+            "55415038757710541115685710749848141603954998263526436372.47"
+    })
+    void testBigDecimal(String input) {
+        var wingspan = new BigDecimal(input);
+        var bird = new CborTestData.BirdBuilder().wingspan(wingspan).build();
+        var ser = CODEC.serialize(bird, SETTINGS);
+        System.out.println(HexFormat.of().formatHex(ByteBufferUtils.getBytes(ser)));
+        var de = new CborTestData.BirdBuilder().deserialize(CODEC.newDeserializer(ser, SETTINGS)).build();
+        assertEquals(wingspan, de.wingspan);
+    }
+
+    @Test
+    void testBigIntegerWithNegativeFirstByte() {
+        var inputs = Map.of(
+                "bf6b666c6967687452616e6765c251fb3fffffffffffffff0000000000000001ff",
+                "85495944688885788945154423373158053576705",
+                "bf6b666c6967687452616e6765c351fb3fffffffffffffff0000000000000001ff",
+                "-85495944688885788945154423373158053576706");
+
+        for (var input : inputs.entrySet()) {
+            byte[] ser = HexFormat.of().parseHex(input.getKey());
+            var de = new CborTestData.BirdBuilder().deserialize(CODEC.newDeserializer(ser, SETTINGS)).build();
+            assertEquals(new BigInteger(input.getValue()), de.flightRange);
+        }
+    }
+
+    @Test
+    void testBigDecimalWithNegativeFirstByte() {
+        var inputs = Map.of(
+                "bf6877696e677370616ec48221c25818e1ffffffffffffff40000000000000017fffffffffffffffff",
+                "55415038757710541115685710749848141603954998263526436372.47",
+                "bf6877696e677370616ec48221c35818e1ffffffffffffff40000000000000017fffffffffffffffff",
+                "-55415038757710541115685710749848141603954998263526436372.48");
+
+        for (var input : inputs.entrySet()) {
+            byte[] ser = HexFormat.of().parseHex(input.getKey());
+            var de = new CborTestData.BirdBuilder().deserialize(CODEC.newDeserializer(ser, SETTINGS)).build();
+            assertEquals(new BigDecimal(input.getValue()), de.wingspan);
+        }
     }
 
     private static void assertBuffersEqual(ByteBuffer expected, ByteBuffer actual) {
